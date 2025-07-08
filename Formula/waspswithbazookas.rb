f84178d02b41def445037d01dc713848fe1b2dfe
Jt Whissel <jtwhissel@gmail.com>
2025-07-08 19:08:42 -0400

    Fix Homebrew formula to build from source instead of using cargo install

--- Formula/waspswithbazookas.rb
+++ Formula/waspswithbazookas.rb
@@ -1,38 +1,25 @@
 class Waspswithbazookas < Formula
-  desc "Distributed load testing tool - like bees with machine guns, but way more power!"
+  desc "Distributed HTTP/S load testing tool with cluster-based architecture"
   homepage "https://github.com/Phara0h/WaspsWithBazookas"
-  version "2.0.1"
-  license "GPL-2.0"
-  
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/Phara0h/WaspsWithBazookas/releases/download/v#{version}/waspswithbazookas-macos-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256" # This will be updated by the release process
-    else
-      url "https://github.com/Phara0h/WaspsWithBazookas/releases/download/v#{version}/waspswithbazookas-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256" # This will be updated by the release process
-    end
-  end
+  url "https://github.com/Phara0h/WaspsWithBazookas/archive/refs/tags/v2.0.1.tar.gz"
+  sha256 "REPLACE_ME_WITH_SHA256"
+  license "GPL-2.0-or-later"
 
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/Phara0h/WaspsWithBazookas/releases/download/v#{version}/waspswithbazookas-linux-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256" # This will be updated by the release process
-    else
-      url "https://github.com/Phara0h/WaspsWithBazookas/releases/download/v#{version}/waspswithbazookas-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256" # This will be updated by the release process
-    end
-  end
+  depends_on "rust" => :build
 
   def install
-    bin.install "hive"
-    bin.install "wasp"
-    bin.install "test-dummy"
+    # Build all binaries from source
+    system "cargo", "build", "--release", "--bin", "hive", "--bin", "wasp", "--bin", "test-dummy"
+    
+    # Install the binaries
+    bin.install "target/release/hive"
+    bin.install "target/release/wasp"
+    bin.install "target/release/test-dummy"
   end
 
   test do
-    system "#{bin}/hive", "--version"
-    system "#{bin}/wasp", "--version"
-    system "#{bin}/test-dummy", "--version"
+    assert_match version.to_s, shell_output("#{bin}/hive --version")
+    assert_match version.to_s, shell_output("#{bin}/wasp --version")
+    assert_match version.to_s, shell_output("#{bin}/test-dummy --version")
   end
-end 
+end
